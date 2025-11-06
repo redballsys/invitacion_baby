a4d62f42d169c44aaf1124a2bd6ce43500c310fa
redballsys <redballsystems@gmail.com>
2025-11-06 14:20:16 -0600

    Delete invitacion-baby directory

--- invitacion-baby/src/App.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import InvitacionBabyShower from './InvitacionBabyShower.jsx'
-
-export default function App() {
-  return <InvitacionBabyShower />
-}
--- invitacion-baby/src/components/ui/card.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export function Card({ className = '', children }) {
-  return <div className={`rounded-xl border bg-white ${className}`}>{children}</div>
-}
-export function CardContent({ className = '', children }) {
-  return <div className={className}>{children}</div>
-}
